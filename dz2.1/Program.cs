fc418d336fb9c79b29123dd45696ffc8a962ca4b
anton <anton@LAPTOP-OBTFOHK5>
2021-10-28 20:44:34 +0300

    Добавьте файлы проекта.

---
+++ dz2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace dz2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Кипятков Иван Петрович";
+            byte age = 17;
+            string email = "kipyatok@gmail.com";
+            float computerScienceScore = 79.84f;
+            float mathScore = 84.9f;
+            float physicsScore = 76.12f;
+
+            WriteLine($"{"ФИО ||",25}{fullName,25}");
+            WriteLine($"{"Возраст ||",25}{age,25}");
+            WriteLine($"{"Email ||",25}{email,25}");
+            WriteLine($"{"Баллы по информатике ||",25}{computerScienceScore,25}");
+            WriteLine($"{"Баллы по математике ||",25}{mathScore,25}");
+            WriteLine($"{"Баллы по физике ||",25}{physicsScore,25}");
+
+            ReadLine();
+        }
+    }
+}
---
+++ dz2.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace dz2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float computerScienceScore;
+            float mathScore;
+            float physicsScore;
+
+            float sumScores;
+            float averageScores;
+
+            WriteLine("Введите баллы по информатике:");
+            computerScienceScore = Convert.ToSingle(ReadLine());
+            WriteLine("Введите баллы по математике:");
+            mathScore = Convert.ToSingle(ReadLine());
+            WriteLine("Введите баллы по физике:");
+            physicsScore = Convert.ToSingle(ReadLine());
+
+            sumScores = computerScienceScore + mathScore + physicsScore;
+            averageScores = sumScores / 3;
+
+            WriteLine("Нажмите на любую кнопку, чтобы вывести информацию по баллам");
+            ReadKey();
+            WriteLine($"{"Баллы по информатике ||",25}{computerScienceScore,10}");
+            WriteLine($"{"Баллы по математике ||",25}{mathScore,10}");
+            WriteLine($"{"Баллы по физике ||",25}{physicsScore,10}");
+            WriteLine($"{"Сумма баллов ||",25}{sumScores,10}");
+            WriteLine($"{"Средний балл ||",25}{averageScores,10}");
+
+            ReadLine();
+        }
+    }
+}
